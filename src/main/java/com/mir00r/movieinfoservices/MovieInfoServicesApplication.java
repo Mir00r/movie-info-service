9a7f6110a70fe74aecbe905a644342eb21587809
razzak007 <razzak@cmedhealth.com>
2022-04-01 17:09:59 +0600

    ïnitiate spring boot project for movie info service

---
+++ src/main/java/com/mir00r/movieinfoservices/MovieInfoServicesApplication.java
@@ -0,0 +1,13 @@
+package com.mir00r.movieinfoservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieInfoServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieInfoServicesApplication.class, args);
+	}
+
+}
